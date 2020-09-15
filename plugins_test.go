18cda121ffa1a617d3b2b8040ac8684098ff79fc
David Minor <dahvid.minor@gmail.com>
2020-09-15 16:52:32 +0300

    First version with plugins working They work via the factory Created type Plugin that adds Info() function for adding arbitrary meta-data to plugins this will be needed for setting input and output options for plugins

--- plugins_test.go
+++ plugins_test.go
@@ -1,41 +1,38 @@
 package goflow
 
 import (
-	"testing"
-	"path"
 	"fmt"
 	"os"
+	"path"
+	"testing"
 )
-
 
 func TestBadParams(t *testing.T) {
 	paths := []string{"boogie"}
-	_,err := LoadComponents(paths, NewFactory())
+	_, err := LoadComponents(paths, NewFactory())
 	if err != nil {
 		t.Error("LoadComponenets succeeded with bad parameters")
 	}
-	paths = []string{"/usr/lib","/lib"}
-	_,err = LoadComponents(paths, NewFactory())
+	paths = []string{"/usr/lib", "/lib"}
+	_, err = LoadComponents(paths, NewFactory())
 	if err != nil {
 		t.Error("LoadComponenets succeeded with bad parameters")
 	}
 }
 
 func TestOpening(t *testing.T) {
-	test_plugs := path.Join(os.Getenv("GOPATH"),"bin")
-	paths := []string{test_plugs}
+	testPlugs := path.Join(os.Getenv("GOPATH"), "bin")
+	paths := []string{testPlugs}
 	factory := NewFactory()
-	plugs,err := LoadComponents(paths, factory)
+	plugs, err := LoadComponents(paths, factory)
 	if err != nil {
-		t.Error("Failed loading compononents in ./test_plugins",err)
+		t.Error("Failed loading compononents in ./test_plugins", err)
 	}
-	fmt.Println("Opened plugs",plugs)
-	any,err := factory.Create("Plug1")
+	fmt.Println("Opened plugs", plugs)
+	any, err := factory.Create("Plug1")
 	if err != nil {
-		t.Error("Failed to create object Plug1",err)
+		t.Error("Failed to create object Plug1", err)
 	}
 	plug1 := any.(PlugIn)
 	fmt.Println(plug1.Info())
-
-
 }
--- test_plugins/Plug1_goplug.go
+++ test_plugins/Plug1_goplug.go
@@ -2,41 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"math/rand"
-	//"github.com/dahvid/goflow"
+	"time"
 )
-
 
 // RandomNumberGenerator Listen for ssh connection
 type RandomNumberGenerator struct {
 	Generator *rand.Rand
-	info map[string]string
-	Out chan<- int //output
+	meta      map[string]string
+	Out       chan<- int //output
 }
-
 
 // Process listen
 func (c *RandomNumberGenerator) Process() {
 	for {
 		i := c.Generator.Intn(100)
-		fmt.Println("Generating",i)
+		fmt.Println("Generating", i)
 		c.Out <- i
 	}
 }
 
+//Info something
 func (c *RandomNumberGenerator) Info() map[string]string {
-	return c.info
+	return c.meta
 }
-
-
 
 //Plug1 something
 func Plug1() (interface{}, error) {
 	seed := rand.NewSource(time.Now().UnixNano())
-    gen := rand.New(seed)
-	r  := new(RandomNumberGenerator)
+	gen := rand.New(seed)
+	r := new(RandomNumberGenerator)
 	r.Generator = gen
-	r.info["How are you?"] = "I'm OK"
-	return r,nil
+	r.meta = make(map[string]string)
+	r.meta["How are you?"] = "I'm OK"
+	return r, nil
 }
